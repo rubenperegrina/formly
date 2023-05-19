5cfa6d55b7492840ba392a0d62d3a33ad853425c
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-05-19 08:28:42 +0200

    Add some inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formly';
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    },
+    {
+      key: 'Input',
+      type: 'input',
+      props: {
+        label: 'Input',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+      },
+    },
+    {
+      key: 'Checkbox',
+      type: 'checkbox',
+      props: {
+        label: 'Accept terms',
+        description: 'In order to proceed, please accept terms',
+        pattern: 'true',
+        required: true,
+      },
+      validation: {
+        messages: {
+          pattern: 'Please accept the terms',
+        },
+      },
+    },
+    {
+      key: 'Radio',
+      type: 'radio',
+      props: {
+        label: 'Radio',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+        options: [
+          { value: 1, label: 'Option 1' },
+          { value: 2, label: 'Option 2' },
+          { value: 3, label: 'Option 3' },
+          { value: 4, label: 'Option 4', disabled: true },
+        ],
+      },
+    },
+  ];
+
+  onSubmit(model:any) {
+    console.log(model);
+  }
 }
